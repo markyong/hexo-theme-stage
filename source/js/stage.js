5f2ddbd0d57f18555d2cd3aeca4d1ae1cdf7ea7e
markyong <mayyyyyyyong@gmail.com>
2018-08-05 14:59:29 +0800

    header update

---
+++ source/js/stage.js
@@ -0,0 +1,6 @@
+// header
+//var headerDom = document.querySelector('header')
+//headerDom.style.height = window.innerHeight * 0.7 + 'px'
+//window.addEventListener('resize', function () {
+//  headerDom.style.height = window.innerHeight * 0.7 + 'px'
+//})

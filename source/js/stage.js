e4cb50189e786416ed087830206834cab6920456
markyong <mayyyyyyyong@gmail.com>
2018-08-10 17:43:26 +0800

    header img update

--- source/js/stage.js
+++ source/js/stage.js
@@ -1,6 +1,27 @@
 // header
-//var headerDom = document.querySelector('header')
-//headerDom.style.height = window.innerHeight * 0.7 + 'px'
-//window.addEventListener('resize', function () {
-//  headerDom.style.height = window.innerHeight * 0.7 + 'px'
-//})
+function initWater () {
+  var settings = {
+    image: '../images/default-header.jpg',
+    rippleRadius: 2,
+    width: 1000,
+    height: 300,
+    delay: 1,
+    auto: true
+  }
+  var navImg = document.getElementById('nav-img')
+  var waterRippleEffect = new WaterRippleEffect(navImg, settings)
+  navImg.addEventListener('click', function (e) {
+    var mouseX = e.layerX
+    var mouseY = e.layerY
+    waterRippleEffect.disturb(mouseX, mouseY)
+  })
+  navImg.addEventListener('mousemove', function (e) {
+    var mouseX = e.layerX
+    var mouseY = e.layerY
+    waterRippleEffect.disturb(mouseX, mouseY)
+  })
+}
+
+window.onload = function () {
+  initWater()
+}
